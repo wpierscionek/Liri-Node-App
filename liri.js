5985963732bbaabc87147418af1bf2ab94cea734
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-04-04 18:03:39 -0400

    4/4/2016

--- liri.js
+++ liri.js
@@ -1,21 +1,69 @@
+//====================
+//Global Variables
+//====================
+var fs = require('fs');
+var keys = require('./keys.js');
 var Twitter = require('twitter');
-
+var userInput = process.argv[2];
+var value = process.argv[3];
+//====================
+//Commands
+//====================
+function checkUserInput(userInput, value) {
+    switch (userInput) {
+        case "my-tweets":
+            twitter();
+            break;
+        case "spotify-this-song":
+            spotify();
+            break;
+        case "movie-this":
+            movie();
+            break;
+        case "do-what-it-says":
+            whatItSays();
+            break;
+    };
+};
+//====================
+//Twitter
+//====================
 var client = new Twitter({
-    consumer_key: 'ooOeVRJvY33357CxhdoV9v5sC',
-    consumer_secret: 'P5NJCq0UXIVQ3gGMLfzdOPjKPHMzr2cAz8j5XRoa3mAqAPXUg6',
-    access_token_key: '707597378677817344-3BciQIbdpHsCKMDuL8df2ZAn5xhuiFg',
-    access_token_secret: 'Ul4jDMB8qHfJKya0GRztjzhi8tG7LQ1F5Rio5oJit8M2h'
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret,
 });
-var count = 0;
-util = require('util');
-client.stream('statuses/filter', { track: 'love' }, function(stream) {
-    stream.on('data', function(data) {
-        console.log(util.inspect(data));
-        // stream.destroy();
-        // process.exit(0);
+
+function twitter() {
+    var params = { screen_name: '@nicolepolakk', count: '20'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            console.log(tweets);
+        };
     });
 
-    stream.on('error', function(error) {
-        throw error;
-    });
-});
+};
+checkUserInput(userInput, value);
+//====================
+//Spotify
+//====================
+
+function spotify(){
+    //do something
+
+}
+//====================
+//Movie
+//====================
+function movie(){
+    //do something
+
+}
+//====================
+//whatIsSays
+//====================
+function whatItSays(){
+    //do something
+
+}
