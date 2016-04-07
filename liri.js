8f8549be9443f2e80eff6a256c378bea235c5819
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-04-07 11:58:21 -0400

    4/7/2016

--- liri.js
+++ liri.js
@@ -17,7 +17,7 @@ function checkUserInput(userInput, value) {
             twitter();
             break;
         case "spotify-this-song":
-            spotify();
+            spotifyThis(value);
             break;
         case "movie-this":
             movie(value);
@@ -50,35 +50,87 @@ checkUserInput(userInput, value);
 //====================
 //Spotify
 //====================
-function spotify() {
-    //do something
-}
+function spotifyThis(songs) {
+
+    spotify.search({ type: 'track', query: songs }, function(err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        };
+        console.log(data);
+        var items = data.tracks.items;
+        for (i = 0; i < items.length; ++i) {
+            console.log("Song Name: " + items[i].name);
+            console.log("Number of albums with this track: " + items.length);
+            console.log("Preview Link of the song on Spotify: " + items[i].preview_url);
+            console.log("Album Name: " + items[i].album.name);
+            console.log(+items[i].artists.length);
+
+           	saveToDocument("Song Name: " + items[i].name);
+           	saveToDocument("Number of albums with this track: " + items.length);
+           	saveToDocument("Preview Link of the song on Spotify: " + items[i].preview_url);
+           	saveToDocument("Album Name: " + items[i].album.name);
+           	saveToDocument(+items[i].artists.length);
+        };
+    });
+};
 //====================
 //Movie
 //====================
-function movie(movie) {
-    //do something
-
-    request('http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&r=json', function(error, response, body) {
+function movie(whatMovie) {
+    request('http://www.omdbapi.com/?t=' + whatMovie + '&y=&plot=short&r=json', function(error, response, body) {
         if (!error && response.statusCode == 200) {
             var json = JSON.parse(body);
-            console.log("Title: "+ json.Title);
-            console.log("Year: "+ json.Year);
-            console.log("IMDB Rating: "+ json.imdbRating);
-            console.log("Country: "+ json.Country);
-            console.log("Language: "+ json.Language);
-            console.log("Plot: "+ json.Plot);
-            console.log("Actors: "+ json.Actors);
-            console.log("Rotten Tomatoes rating: "+ json.tomatoRating);
-            console.log("Rotten Tomatoes URL: "+ json.tomatoURL);
-        }
-    })
+            console.log("Title: " + json.Title);
+            console.log("Year: " + json.Year);
+            console.log("IMDB Rating: " + json.imdbRating);
+            console.log("Country: " + json.Country);
+            console.log("Language: " + json.Language);
+            console.log("Plot: " + json.Plot);
+            console.log("Actors: " + json.Actors);
+            console.log("Rotten Tomatoes rating: " + json.tomatoRating);
+            console.log("Rotten Tomatoes URL: " + json.tomatoURL);
 
-}
+            saveToDocument("Title: " + json.Title);
+            saveToDocument("Year: " + json.Year);
+            saveToDocument("IMDB Rating: " + json.imdbRating);
+            saveToDocument("Country: " + json.Country);
+            saveToDocument("Language: " + json.Language);
+            saveToDocument("Plot: " + json.Plot);
+            saveToDocument("Actors: " + json.Actors);
+            saveToDocument("Rotten Tomatoes rating: " + json.tomatoRating);
+            saveToDocument("Rotten Tomatoes URL: " + json.tomatoURL);
+        };
+    });
+
+};
 //====================
 //whatItSays
 //====================
 function whatItSays() {
-    //do something
+    fs.readFile("random.txt", "utf-8", function read(err, data) {
+        if (err) {
+            return console.log(err);
+        };
+        if (data.indexOf(",") > 0) {
+            var myOwnProcessArgv = data.split(",");
+            value = myOwnProcessArgv[1];
+            userInput = myOwnProcessArgv[0];
+        };
 
-}
+        checkUserInput(userInput, value);
+
+    });
+};
+//====================
+//Save to document
+//====================
+function saveToDocument(string){
+	
+	fs.appendFile('output.txt', string, function(err) {
+		  if ( err ) {
+		        console.log('Error occurred: ' + err);
+		        return;
+};
+});
+};
